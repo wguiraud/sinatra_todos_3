67c4900d37b4a459124e3b1bf85fb00684ca62e5
William <wguiraud@protonmail.com>
2024-09-17 17:03:46 -0700

    how do you create a new list

--- todo.rb
+++ todo.rb
@@ -2,18 +2,30 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-set :session_secret, SecureRandom.hex(32)
+configure do 
+  enable :sessions
+  set :session_secret, 'hello world I need a 64 bytes session password in order to make sinatra happy' #ok for development env but not ok for production
+  #set :session_secret, SecureRandom.hex(32)
+end
+
+before do 
+  session[:lists] ||= []
+end
 
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do 
-  @lists = [ 
-    { name: :list1, todos: [ {}, {}, {}] }, 
-    { name: :list2, todos: [ {} ] }
-  ]
+  @lists = session[:lists] #By default, the session hash should reference an empty array if no lists have been created yet. If not the call to Enumerator#each occuring in the lists.erb view template will raise a NoMethodError!
   erb :lists
 end
 
+get "/lists/new" do 
+  erb :new_list
+end
 
+post "/lists" do
+  list_name = params[:list_name]
+  session[:lists] << { name: list_name, todos: [] }
+end
