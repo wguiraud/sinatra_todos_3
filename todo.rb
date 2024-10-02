772cf679c47cf79df6f9f9247a339ba7b4d9b5f1
William <wguiraud@protonmail.com>
2024-10-01 17:20:43 -0700

    "adding a todo to a list"

--- todo.rb
+++ todo.rb
@@ -12,26 +12,32 @@ configure do
   # set :session_secret, SecureRandom.hex(32)
 end
 
-before do
-  session[:lists] ||= []
-end
-
 def non_unique_name?(given_list_name)
   used_names = []
   session[:lists].each { |list| used_names << list[:name] }
   used_names.include?(given_list_name)
 end
 
-def invalid_list_name?(list_name)
+def invalid_name?(list_name)
   !list_name.match?(/^(?!.*([.?!@#$%\^&*()\-_+=\[\]{}|;:<>]).*\1)[A-Za-z0-9\s,.?!@#$%\^&*()\-_+=\[\]{}|;:<>]{1,100}$/)
 end
 
 def error_for_list_name(list_name)
-  if invalid_list_name?(list_name)
+  if invalid_name?(list_name)
     'Please enter a valid list name.'
   elsif non_unique_name?(list_name)
     'Sorry, this list name is already being used.'
   end
+end
+
+def error_for_todo_name(todo_name)
+  if invalid_name?(todo_name)
+    'Please enter a valid todo name.'
+  end
+end
+
+before do
+  session[:lists] ||= []
 end
 
 get '/' do
@@ -45,6 +51,38 @@ end
 
 get '/lists/new' do
   erb :new_list
+end
+
+post "/lists" do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "#{list_name} was successfully created"
+    redirect '/lists'
+  end
+end
+
+get "/lists/:list_id" do
+  ### Using instance variables to pass data to the templates
+  # @list_id = params[:list_id].to_i
+  # @list = session[:lists][@list_id]
+  # @list_name = @list[:name]
+  ### Using a hash of local variables in conjunction with the locals option to pass data down to the templates
+  list_id = params[:list_id].to_i
+  list = session[:lists][list_id]
+  list_name = list[:name]
+  erb :list, locals: { list_id: list_id, list: list, list_name: list_name }
+  #erb :list
+end
+
+get "/lists/:list_id/edit" do 
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  erb :edit_list
 end
 
 post "/lists/:list_id" do 
@@ -62,32 +100,41 @@ post "/lists/:list_id" do
   end
 end
 
-post "/lists" do
-  list_name = params[:list_name].strip
-  error = error_for_list_name(list_name)
+post "/lists/:list_id/delete" do 
+  list_id = params[:list_id].to_i
+  list_name = session[:lists][list_id][:name]
+  session[:lists].delete_at(list_id)
+  session[:success] = "#{list_name} was successfully deleted"
+  redirect "/lists"
+end
+
+post "/lists/:list_id/todos" do
+  #Using instance variables to pass data to the templates
+#  @list_id = params[:list_id].to_i
+#  todo_name = params[:todo_name].strip
+#  @list = session[:lists][@list_id]
+#  @list_name = @list[:name]
+  # Using a hash of local variables in conjunction with the locals option to
+  # pass data down to the templates
+  list_id = params[:list_id].to_i
+  list = session[:lists][list_id]
+  list_name = list[:name]
+
+  todo_name = params[:todo_name].strip
+
+
+
+    error = error_for_todo_name(todo_name)
   if error
     session[:error] = error
-    erb :new_list
+    erb :list, locals: { list_id: list_id, list: list, list_name: list_name }
+    #erb :list
   else
-    session[:lists] << { name: list_name, todos: [] }
-    session[:success] = "#{list_name} was successfully created"
-    redirect '/lists'
+    #@list[:todos] << { name: todo_name, completed: false }
+    list[:todos] << { name: todo_name, completed: false }
+    session[:success] = "The todo was successfully created!"
+    redirect "/lists/#{list_id}"
   end
 end
-
-get "/lists/:list_id/edit" do 
-  list_id = params[:list_id].to_i
-  @list = session[:lists][list_id]
-  erb :edit_list
-end
-
-
-get "/lists/:list_id" do 
-  @list_id = params[:list_id].to_i
-  list = session[:lists][@list_id]
-  @list_name = list[:name] 
-  @list_todos = list[:todos]
-  erb :list
-end 
 
 
