ef0a47ca6beedb7d47fe949d5f4fc571cce10951
William <wguiraud@protonmail.com>
2024-10-22 10:58:30 -0700

    "adding javascript"

---
+++ public/javascripts/application.js
@@ -0,0 +1,15 @@
+// application.js
+
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+
+});
